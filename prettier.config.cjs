34bc3e36a5d0f53c443f0d865534028bd13090da
Kelvin Mai <kelvin.mai002@gmail.com>
2024-01-27 15:49:05 -0800

    setup configs

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 100,
+  singleAttributePerLine: true,
+};
